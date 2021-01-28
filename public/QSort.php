289b5d7b3812f5de3cf024d58bc2aef335adfbdc
Tim Smith <tsmith39@tulane.edu>
2021-01-28 15:13:38 -0600

    Works?

--- public/QSort.php
+++ public/QSort.php
@@ -1,29 +1,5 @@
 <?php
 
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
-function partition($arr, $low, $high) {
-    $pivot = $arr[$high]; // pivot
-    $i = $low-1; // Index of smaller element
-
-    for ($j = $low; $j < $high; $j++) {
-        // If current element is smaller than the pivot
-        if ($arr[$j] < $pivot) {
-            $i++; // increment index of smaller element
-            $temp = $arr[$i];
-            $arr[$i] = $arr[$j];
-            $arr[$j] = $temp;
-        }
-    }
-    $temp = $arr[$i+1];
-    $arr[$i+1] = $arr[$high];
-    $arr[$high] = $temp;
-    return $i+1;
-}
-
 /* The main function that implements QuickSort
 arr[] --> Array to be sorted,
 low --> Starting index,
@@ -32,7 +8,22 @@ function quickSort($arr, $low, $high) {
     if ($low < $high) {
         /* pi is partitioning index, arr[p] is now
         at right place */
-        $pi = partition($arr, $low, $high);
+        $pivot = $arr[$high]; // pivot
+        $i = $low-1; // Index of smaller element
+
+        for ($j = $low; $j < $high; $j++) {
+            // If current element is smaller than the pivot
+            if ($arr[$j] < $pivot) {
+                $i++; // increment index of smaller element
+                $temp = $arr[$i];
+                $arr[$i] = $arr[$j];
+                $arr[$j] = $temp;
+            }
+        }
+        $temp = $arr[$i+1];
+        $arr[$i+1] = $arr[$high];
+        $arr[$high] = $temp;
+        $pi = $i + 1;
 
         // Separately sort elements before
         // partition and after partition
@@ -44,5 +35,5 @@ function quickSort($arr, $low, $high) {
 
 $items = ['Jimmy', 9, 'Tim', 8, 'Reese', 7, 'butts'];
 $n = count($items);
-quickSort($items, 0, $n - 1);
+$items = quickSort($items, 0, $n - 1);
 print_r($items);
