8bbdc1ac1e0422294d9f59b9c49ee246945ff49a
Tim Smith <tsmith39@tulane.edu>
2021-01-28 14:29:30 -0600

    stil working

--- public/QSort.php
+++ public/QSort.php
@@ -1,12 +1,5 @@
 <?php
 
-// A utility function to swap two elements
-function swap($a, $b) {
-    $t = $a;
-    $a = $b;
-    $b = $t;
-}
-
 /* This function takes last element as pivot, places
 the pivot element at its correct position in sorted
 array, and places all smaller (smaller than pivot)
@@ -16,14 +9,18 @@ function partition($arr, $low, $high) {
     $pivot = $arr[$high]; // pivot
     $i = $low-1; // Index of smaller element
 
-    for ($j = $low; $j <= $high-1; $j++) {
+    for ($j = $low; $j < $high; $j++) {
         // If current element is smaller than the pivot
         if ($arr[$j] < $pivot) {
             $i++; // increment index of smaller element
-            swap($arr[$i], $arr[$j]);
+            $temp = $arr[$i];
+            $arr[$i] = $arr[$j];
+            $arr[$j] = $temp;
         }
     }
-    swap($arr[$i+1], $arr[$high]);
+    $temp = $arr[$i+1];
+    $arr[$i+1] = $arr[$high];
+    $arr[$high] = $temp;
     return $i+1;
 }
 
@@ -39,12 +36,12 @@ function quickSort($arr, $low, $high) {
 
         // Separately sort elements before
         // partition and after partition
-        quickSort($arr, $low, $pi - 1);
-        quickSort($arr, $pi + 1, $high);
+        quickSort($arr, $low, $pi-1);
+        quickSort($arr, $pi+1, $high);
     }
 }
 
 $items = ['Jimmy', 9, 'Tim', 8, 'Reese', 7, 'butts'];
 $n = count($items);
-$items = quickSort($items, 0, $n - 1);
+quickSort($items, 0, $n - 1);
 print_r($items);
