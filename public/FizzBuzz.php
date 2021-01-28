c6cf651a1847b23af8cfe1c7602ee4200d0150cd
Tim Smith <tsmith39@tulane.edu>
2021-01-28 09:32:36 -0600

    Rename file

---
+++ public/FizzBuzz.php
@@ -0,0 +1,17 @@
+<?php
+
+for($i = 1; $i<=100; $i++){
+    if($i%3 == 0 && $i%5 == 0){
+        echo "FizzBuzz<br>";
+        continue;
+    }
+    if($i%3 == 0){
+        echo "Fizz<br>";
+        continue;
+    }
+    if($i%5 == 0){
+        echo "Buzz<br>";
+        continue;
+    }
+    echo $i."<br>";
+}
