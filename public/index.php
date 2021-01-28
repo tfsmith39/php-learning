240a4e00989be417e79fd5a68da09d25d6ae0968
Tim Smith <tsmith39@tulane.edu>
2021-01-28 09:09:05 -0600

    Fixed numbers to 3/5 + only if statements

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 
 for($i = 0; $i<100; $i++){
-    if($i%2 == 0 && $i%3 == 0){
-        echo "Fizzbuzz\n";
+    if($i%3 == 0 && $i%5 == 0){
+        echo "FizzBuzz\n";
     }
-    elseif($i%3 == 0){
+    if($i%3 == 0 && $i%5 != 0){
         echo "Buzz\n";
     }
-    elseif($i%2 == 0){
+    if($i%3 != 0 && $i%5 == 0){
         echo "Fizz\n";
     }
-    else{
+    if($i%3 != 0 && $i%5 != 0){
         echo $i."\n";
     }
 }
