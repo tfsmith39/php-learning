bc0bb5d4853b5b608e81223d0c53d7621adf83cf
Tim Smith <tsmith39@tulane.edu>
2021-01-28 13:31:25 -0600

    Initial program (not working)

---
+++ public/QSort.php
@@ -0,0 +1,58 @@
+<?php
+
+// A utility function to swap two elements
+function swap(&$a, &$b) {
+    $t = $a;
+    $a = $b;
+    $b = $t;
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
+function partition($arr, $low, $high) {
+    $pivot = $arr[$high]; // pivot
+    $i = $low-1; // Index of smaller element
+
+    for ($j = $low; $j <= $high-1; $j++) {
+        // If current element is smaller than the pivot
+        if ($arr[$j] < $pivot) {
+            $i++; // increment index of smaller element
+            swap($arr[$i], $arr[$j]);
+        }
+    }
+    swap($arr[$i+1], $arr[$high]);
+    return $i+1;
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+function quickSort($arr, $low, $high) {
+    if ($low < $high) {
+        /* pi is partitioning index, arr[p] is now
+        at right place */
+        $pi = partition($arr, $low, $high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort($arr, $low, $pi - 1);
+        quickSort($arr, $pi + 1, $high);
+    }
+}
+
+/* Function to print an array */
+function printArray($arr, $size) {
+    for ($i=0; $i < $size; $i++)
+        echo($arr[$i]);
+    echo("\n");
+}
+
+$items = ['Jimmy', 9, 'Tim', 8, 'Reese', 7, 'butts'];
+$n = count($items);
+quickSort($items, 0, $n - 1);
+printf("Sorted array: \n");
+printArray($items, $n);
