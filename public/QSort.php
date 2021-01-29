b0e65838e6e731233adb51349d0550c20d372a44
Tim Smith <tsmith39@tulane.edu>
2021-01-29 08:41:06 -0600

    I caved and used a reference

--- public/QSort.php
+++ public/QSort.php
@@ -1,39 +1,48 @@
 <?php
 
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-function quickSort($arr, $low, $high) {
-    if ($low < $high) {
-        /* pi is partitioning index, arr[p] is now
-        at right place */
-        $pivot = $arr[$high]; // pivot
-        $i = $low-1; // Index of smaller element
+function partition(&$arr,$low,$high) {
+    $i = ($low - 1);         # index of smaller element
+    $pivot = $arr[$high];     # pivot
 
-        for ($j = $low; $j < $high; $j++) {
-            // If current element is smaller than the pivot
-            if ($arr[$j] < $pivot) {
-                $i++; // increment index of smaller element
-                $temp = $arr[$i];
-                $arr[$i] = $arr[$j];
-                $arr[$j] = $temp;
-            }
+    for($j=$low; $j<$high; $j++){
+
+        # If current element is smaller than the pivot
+        if($arr[$j] <= $pivot) {
+
+            # increment index of smaller element
+            $i = $i + 1;
+            $temp = $arr[$i];
+            $arr[$i] = $arr[$j];
+            $arr[$j] = $temp;
         }
-        $temp = $arr[$i+1];
-        $arr[$i+1] = $arr[$high];
-        $arr[$high] = $temp;
-        $pi = $i + 1;
-
-        // Separately sort elements before
-        // partition and after partition
-        $arr = quickSort($arr, $low, $pi-1);
-        $arr = quickSort($arr, $pi+1, $high);
     }
-    return $arr;
+    $temp = $arr[$i + 1];
+    $arr[$i + 1] = $arr[$high];
+    $arr[$high] = $temp;
+    return $i+1;
 }
 
-$items = ['Jimmy', 9, 'Tim', 8, 'Reese', 7, 'butts'];
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+
+# Function to do Quick sort
+function quickSort(&$arr,$low,$high) {
+    if($low < $high) {
+
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        $pi = partition($arr, $low, $high);
+
+        # Separately sort elements before
+        # partition and after partition
+        quickSort($arr, $low, $pi - 1);
+        quickSort($arr, $pi + 1, $high);
+    }
+}
+
+$items = ['Jimmy', 9, 'Tim', 8, 'Reese', 7, 'Butts'];
 $n = count($items);
-$items = quickSort($items, 0, $n - 1);
+quickSort($items, 0, $n - 1);
 print_r($items);
